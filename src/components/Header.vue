5d5f79609c051aef90f9018b042f69abb711e1aa
gramos <gerard.ramosm89@gmail.com>
2017-05-05 09:39:24 -0700

    changes

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+      <router-link to="/portfolio" activeClass="active" tag="li" class="nav-item"><a class="nav-link">Portfolio</a></router-link>
+      <router-link to="/stocks" activeClass="active" tag="li" class="nav-item"><a class="nav-link">Stocks</a></router-link>
+<!--
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown link
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+-->
+      </ul>
+    </div>
+  </nav>
+</template>
---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="col-md-6">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input type="number"
+          class="form-control"
+          placeholder="Quantity"
+          v-model="quantity" >
+        </div>
+        <div class="pull-right">
+          <button
+          :disabled="quantity <= 0" 
+          class="btn btn-success" 
+          @click="buyStock">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock() {
+        console.log("Buy Stock!");
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: parseInt(this.quantity)
+        };
+        console.log("The order is: ", order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
